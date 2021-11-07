e307a6458b2cafecd12094d2d4870fda848381a2
Kyle Petrovich <1054402+fozzle@users.noreply.github.com>
2021-11-06 20:12:36 -0700

    hero emoji question mark (#7)

--- src/durable-object/postMatch.ts
+++ src/durable-object/postMatch.ts
@@ -3,7 +3,22 @@ import HERO_MAP from '../data/heroes'
 import fetchClient from '../utils/fetchClient'
 import { Match, Player } from '../types/odota'
 import { PlayerConfig } from '../types/shared'
-import { LOSS_COLOR, MESSAGE_ENDPOINT, OPEN_DOTA_MATCH_URL, WIN_COLOR } from './constants'
+import {
+  LOSS_COLOR,
+  MESSAGE_ENDPOINT,
+  OPEN_DOTA_MATCH_URL,
+  WIN_COLOR,
+} from './constants'
+
+function getHeroEmojiString(heroId: number | string): string {
+  const heroEmoji = HERO_MAP[heroId]?.emoji
+  if (!heroEmoji) return ''
+  return `<${heroEmoji}>`
+}
+
+function getHeroName(heroId: number | string): string {
+  return HERO_MAP[heroId]?.name ?? 'Unknown Hero'
+}
 
 export default async function postMatch(
   match: Match,
@@ -56,8 +71,8 @@ export default async function postMatch(
         hero_id: heroId,
       }) => ({
         name: accountAlias(accountId) || personaname || name || `${accountId}`,
-        value: `${
-          HERO_MAP[heroId]?.name ?? 'Unknown Hero'
+        value: `${getHeroEmojiString(heroId)}${
+          getHeroName(heroId)
         } - ${kills}/${deaths}/${assists}`,
         inline: true,
       }),
